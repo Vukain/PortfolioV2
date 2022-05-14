0e8d3f17331137c70fc462171411c2cb3edc6aea
Michal Piechowiak <nilfenheim@gmail.com>
2022-05-14 14:54:43 +0200

    initial commit

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <p>blablabla</p>
+      <Header></Header>
     </div>
   );
 }
