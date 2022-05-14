265ef28b673105ade83039688b0bc53ebe90a0ea
Michal Piechowiak <nilfenheim@gmail.com>
2022-05-14 14:56:39 +0200

    initial commit

---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import bemCssModules from 'bem-css-modules';
+
+import headerStyles from './Header.module.sass';
+
+const Header: React.FC = (props) => {
+
+    const style = bemCssModules(headerStyles);
+
+    return (
+        <div className={style()}>
+            Hello
+        </div>
+    );
+}
+
+export default Header;
