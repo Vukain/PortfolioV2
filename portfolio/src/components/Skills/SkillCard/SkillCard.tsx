d53f7f782a1f73eba1be02a349dc5f70142a82dc
Michal Piechowiak <nilfenheim@gmail.com>
2022-06-15 18:18:03 +0200

    add: card back

--- portfolio/src/components/Skills/SkillCard/SkillCard.tsx
+++ portfolio/src/components/Skills/SkillCard/SkillCard.tsx
@@ -2,6 +2,10 @@ import bemCssModules from 'bem-css-modules';
 
 import skillCardStyles from './SkillCard.module.sass';
 
+import { ReactComponent as FrontendIcon } from '../../../media/icons/adjustments.svg';
+import { ReactComponent as BackendIcon } from '../../../media/icons/tools-2.svg';
+import { ReactComponent as GraphicIcon } from '../../../media/icons/tools.svg';
+
 interface MyProps {
     position: 'first' | 'second' | 'third',
 };
@@ -20,17 +24,29 @@ const SkillCard: React.FC<MyProps> = (props: MyProps) => {
     // const third: boolean = props.position === 'third';
     // const position: Position = { first, second, third };
 
-    const titles = { first: 'frontend', second: 'backend', third: 'grafika' };
+    type CardData = { [key: string]: { title: string, icon: JSX.Element, skills: string[] } };
 
-    // const titles: { [key: string]: string } = { first: 'frontend', second: 'backend', third: 'grafika' }
-    // const titles: { first: string, second: string, third: string } = { first: 'frontend', second: 'backend', third: 'grafika' }
+    const cardData: CardData = {
+        first: { title: 'frontend', icon: <FrontendIcon className={styles('icon_image', position)} />, skills: ['html', 'css', 'sass', 'bootstrap', 'javascript', 'typescript', 'react', 'redux'] },
+        second: { title: 'backend', icon: <BackendIcon className={styles('icon_image', position)} />, skills: ['node.js', 'next.js', 'python', 'django', 'mongodb', 'sqlite'] },
+        third: { title: 'grafika', icon: <GraphicIcon className={styles('icon_image', position)} />, skills: ['adobe photoshop', 'adobe illustrator', 'adobe xd', 'autocad'] }
+    };
 
     return (
         <div className={styles()}>
             <div className={styles('front', position)}>
-                <h2 className={styles('title', position)}>{titles[props.position]}</h2>
+                <h2 className={styles('title', position)}>{cardData[props.position].title}</h2>
+
             </div>
-            <div className={styles('back')}></div>
+            <div className={styles('back', position)}>
+                <h2 className={styles('title', { ...position, back: true })}>{cardData[props.position].title}</h2>
+                <ul className={styles('skills')}>
+                    {cardData[props.position].skills.map(skill => <li className={styles('skill')}>{skill}</li>)}
+                </ul>
+                <div className={styles('icon')}>
+                    {cardData[props.position].icon}
+                </div>
+            </div>
         </div>
     );
 };
