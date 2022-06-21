d46c39aca759204c5607133fac1c118075e1f279
Michal Piechowiak <nilfenheim@gmail.com>
2022-06-21 14:15:10 +0200

    styling: cleanup

--- portfolio/src/components/Skills/SkillCard/SkillCard.tsx
+++ portfolio/src/components/Skills/SkillCard/SkillCard.tsx
@@ -1,53 +1,42 @@
+import * as React from 'react';
+
 import bemCssModules from 'bem-css-modules';
 
 import skillCardStyles from './SkillCard.module.sass';
 
-import { ReactComponent as FrontendIcon } from '../../../media/icons/adjustments.svg';
-import { ReactComponent as BackendIcon } from '../../../media/icons/tools-2.svg';
-import { ReactComponent as GraphicIcon } from '../../../media/icons/tools.svg';
-
 interface MyProps {
-    position: 'first' | 'second' | 'third',
+    data: { title: string, skills: string[] },
+    children: JSX.Element
 };
 
 const SkillCard: React.FC<MyProps> = (props: MyProps) => {
 
     const styles = bemCssModules(skillCardStyles);
 
-    // type Position = { first?: boolean, second?: boolean, third?: boolean };
-    // const position: Position = {};
-    // const position: { [key: string]: boolean } = {};\
+    const position: Record<MyProps['data']['title'], true> = { [props.data.title]: true };
 
-    const position: Partial<Record<MyProps['position'], boolean>> = {};
-
-    position[props.position] = true;
-
-    // type CardData = { [key: string]: { title: string, icon: JSX.Element, skills: string[] } };
-
-    type CardData = Record<MyProps['position'], { title: string, icon: JSX.Element, skills: string[] }>;
-
-    const cardData: CardData = {
-        first: { title: 'frontend', icon: <FrontendIcon className={styles('icon_image', position)} />, skills: ['html', 'css', 'sass', 'bootstrap', 'javascript', 'typescript', 'react', 'redux'] },
-        second: { title: 'backend', icon: <BackendIcon className={styles('icon_image', position)} />, skills: ['node.js', 'next.js', 'python', 'django', 'mongodb', 'sqlite'] },
-        third: { title: 'grafika', icon: <GraphicIcon className={styles('icon_image', position)} />, skills: ['adobe photoshop', 'adobe illustrator', 'adobe xd', 'autocad'] }
-    };
+    // const position: Partial<Record<MyProps['data']['title'], boolean>> = {};
+    // position[props.data.title] = true;
 
     return (
         <div className={styles()}>
+
             <div className={styles('front', position)}>
-                <h2 className={styles('title', position)}>{cardData[props.position].title}</h2>
+                <h2 className={styles('title', position)}>{props.data.title}</h2>
             </div>
+
             <div className={styles('back', position)}>
                 <div className={styles('skew_wrapper')}>
-                    <h2 className={styles('title', { ...position, back: true })}>{cardData[props.position].title}</h2>
+                    <h2 className={styles('title', { ...position, back: true })}>{props.data.title}</h2>
                     <ul className={styles('skills')}>
-                        {cardData[props.position].skills.map((skill, idx) => <li className={styles('skill', position)} key={idx}>{skill}</li>)}
+                        {props.data.skills.map((skill, idx) => <li className={styles('skill', position)} key={idx}>{skill}</li>)}
                     </ul>
-                    <div className={styles('icon')}>
-                        {cardData[props.position].icon}
-                    </div>
+                </div>
+                <div className={styles('image')}>
+                    {props.children}
                 </div>
             </div>
+
         </div>
     );
 };
--- portfolio/src/components/Skills/Skills.tsx
+++ portfolio/src/components/Skills/Skills.tsx
@@ -4,15 +4,33 @@ import skillsStyles from './Skills.module.sass';
 
 import SkillCard from './SkillCard/SkillCard';
 
+import { ReactComponent as FrontendIcon } from '../../media/icons/adjustments.svg';
+import { ReactComponent as BackendIcon } from '../../media/icons/tools-2.svg';
+import { ReactComponent as GraphicIcon } from '../../media/icons/tools.svg';
+
 const Skills: React.FC = (props) => {
 
     const style = bemCssModules(skillsStyles);
 
+    type CardData = Record<'frontend' | 'backend' | 'grafika', { title: string, skills: string[] }>;
+
+    const cardData: CardData = {
+        frontend: { title: 'frontend', skills: ['html', 'css', 'sass', 'bootstrap', 'javascript', 'typescript', 'react', 'redux'] },
+        backend: { title: 'backend', skills: ['node.js', 'next.js', 'python', 'django', 'mongodb', 'sqlite'] },
+        grafika: { title: 'grafika', skills: ['adobe photoshop', 'adobe illustrator', 'adobe xd', 'autocad'] }
+    };
+
     return (
         <section className={style()}>
-            <SkillCard position='first' ></SkillCard>
-            <SkillCard position='second' ></SkillCard>
-            <SkillCard position='third' ></SkillCard>
+            <SkillCard data={cardData['frontend']}>
+                <FrontendIcon className={style("icon", { frontend: true })} title="frontend icon" />
+            </SkillCard>
+            <SkillCard data={cardData['backend']}>
+                <BackendIcon className={style("icon", { backend: true })} title="backend icon" />
+            </SkillCard>
+            <SkillCard data={cardData['grafika']}>
+                <GraphicIcon className={style("icon", { grafika: true })} title="graphics icon" />
+            </SkillCard>
         </section>
     );
 }
