caf585116aa0c88436a280499759ef976235697f
Michal Pi <nilfenheim@gmail.com>
2022-07-03 02:11:32 +0200

    sty: code cleanup

--- portfolio/src/components/Projects/Projects.tsx
+++ portfolio/src/components/Projects/Projects.tsx
@@ -4,12 +4,12 @@ import styles from './Projects.module.sass';
 
 type MyProps = JSX.IntrinsicElements["div"] & { test?: string };
 
-const Projects = forwardRef<HTMLDivElement, MyProps>((props, ref) => {
+const Projects = forwardRef<HTMLDivElement, MyProps>(({ test }, ref) => {
 
     return (
 
         <div className={styles.projects} ref={ref}>
-            <div className={styles.project}>{props.test}</div>
+            <div className={styles.project}>{test}</div>
             <div className={styles.project}></div>
             <div className={styles.project}></div>
         </div>
