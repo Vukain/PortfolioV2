0199685ebba502fee9991a63a31a602c4068b917
Michal Piechowiak <nilfenheim@gmail.com>
2022-08-13 11:48:18 +0200

    style: code cleanup

--- portfolio/src/components/Contact/ParallaxCrystal/ParallaxCrystal.tsx
+++ portfolio/src/components/Contact/ParallaxCrystal/ParallaxCrystal.tsx
@@ -1,19 +1,22 @@
-import { JsxElement } from 'typescript';
 import styles from './ParallaxCrystal.module.sass';
 
 type MyProps = {
     cursorXPosition: number,
     cursorYPosition: number,
     speed: number,
-    Image: React.ReactNode,
+    index: number,
+    children: React.ReactNode,
 };
 
-const ParallaxCrystal: React.FC<MyProps> = ({ speed, cursorXPosition, cursorYPosition, Image }) => {
+const ParallaxCrystal: React.FC<MyProps> = ({ speed, cursorXPosition, cursorYPosition, index }, children) => {
+
+    const Image = children;
 
     return (
-        <div className={styles.footer} style={{ transform: `translate(${speed * (window.innerWidth - cursorXPosition) / 100}px, ${speed * (window.innerHeight - cursorYPosition) / 100}px)` }}>
-            {/* <Image /> */}
+        <div key={index} className={styles.crystal_wrapper} style={{ transform: `translate(${speed * (window.innerWidth - cursorXPosition) / 100}px, ${speed * (window.innerHeight - cursorYPosition) / 100}px)` }}>
+            <Image className={styles.floating_crystal} />
         </div>
+
     );
 }
 
